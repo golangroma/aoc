d8517638895b1e00102b0bad57c3d5d5ef42490d
Alessio Perugini <alessioper.98@gmail.com>
2022-12-01 14:48:44 +0100

    day1 (#15)

---
+++ 2022/day01/alessio-perugini/parts_test.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/golangroma/aoc/utils"
+)
+
+var sample string = `1000
+2000
+
+4000
+6000
+
+2000
+1000`
+
+func TestPartOne(t *testing.T) {
+	tt := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{
+			name:     "example",
+			input:    sample,
+			expected: "10000",
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			input := utils.SplitInput(tc.input)
+			if got := PartOne(input); got != tc.expected {
+				t.Errorf("PartOne() = %v, want %v", got, tc.expected)
+			}
+		})
+	}
+}
+
+func TestPartTwo(t *testing.T) {
+	tt := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{
+			name:     "example",
+			input:    sample,
+			expected: "16000",
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			input := utils.SplitInput(tc.input)
+			if got := PartTwo(input); got != tc.expected {
+				t.Errorf("PartTwo() = %v, want %v", got, tc.expected)
+			}
+		})
+	}
+}
