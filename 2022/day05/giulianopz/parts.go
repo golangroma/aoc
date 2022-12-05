af3207082ab79234673795f6c47d7b2d46ca8366
giuliano <panzironi.giuliano@gmail.com>
2022-12-05 13:50:42 +0100

    solve part 1

---
+++ 2022/day05/giulianopz/parts.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func prepend(element string, data []string) []string {
+	return append([]string{element}, data...)
+}
+
+func PartOne(input []string) string {
+
+	re, _ := regexp.Compile(`\w+\s(\d+)`)
+
+	crates := make(map[int][]string, 0)
+	row := 0
+	col := 7
+	for _, line := range input {
+
+		if strings.Index(line, "[") != -1 {
+			for i := 0; i < len(line); i += 4 {
+				if string(line[i]) == "[" {
+					letter := string(line[i+1])
+					if letter != "" {
+						elements, ok := crates[row]
+						if !ok {
+							crates[row] = make([]string, 0)
+						}
+						crates[row] = prepend(letter, elements)
+					}
+				}
+				row++
+			}
+			row = 0
+			col--
+		}
+
+		if re.MatchString(line) {
+
+			matches := re.FindAllStringSubmatch(line, -1)
+			fmt.Println(matches)
+
+			num := asInt(matches[0][1])
+			from := asInt(matches[1][1]) - 1
+			to := asInt(matches[2][1]) - 1
+
+			source := crates[from]
+
+			moving := source[len(source)-num:]
+			for len(moving) != 0 {
+				e := moving[len(moving)-1]
+				crates[to] = append(crates[to], e)
+				moving = moving[:len(moving)-1]
+			}
+			crates[from] = source[:len(source)-num]
+		}
+	}
+
+	var top string
+	for i := 0; i < len(crates); i++ {
+		top += crates[i][len(crates[i])-1]
+	}
+	return top
+}
+
+func asInt(s string) int {
+	n, err := strconv.Atoi(s)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return n
+}
+
+func PartTwo(input []string) string {
+	return ""
+}
