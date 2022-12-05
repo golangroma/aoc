dbef0bf1546ba30a8444fcba140ef7015a37027c
Giuliano Panzironi <45801466+giulianopz@users.noreply.github.com>
2022-12-05 17:28:50 +0100

    day 04 (#40)
    * add me
    
    * add solutions to day 01 problems
    
    * add solutions to day 02 problems
    
    * solve part one
    
    * solve part 2
    
    * solve day 04 puzzles
    
    * solve day 04 puzzles

---
+++ 2022/day04/giulianopz/parts.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+)
+
+func PartOne(input []string) string {
+
+	var fullyContainedAssignmentPairs int
+	for _, line := range input {
+		pair := strings.Split(line, ",")
+		firstElf := strings.Split(pair[0], "-")
+		secondElf := strings.Split(pair[1], "-")
+
+		if (asInt(firstElf[0]) <= asInt(secondElf[0]) && asInt(firstElf[1]) >= asInt(secondElf[1])) ||
+			(asInt(secondElf[0]) <= asInt(firstElf[0]) && asInt(secondElf[1]) >= asInt(firstElf[1])) {
+			fullyContainedAssignmentPairs += 1
+		}
+	}
+	return fmt.Sprintf("%d", fullyContainedAssignmentPairs)
+}
+
+func PartTwo(input []string) string {
+
+	var overlappingAssignmentPairs int
+	for _, line := range input {
+		pair := strings.Split(line, ",")
+		firstElf := strings.Split(pair[0], "-")
+		secondElf := strings.Split(pair[1], "-")
+
+		if (asInt(firstElf[0]) <= asInt(secondElf[0]) && asInt(firstElf[1]) >= asInt(secondElf[1])) ||
+			(asInt(secondElf[0]) <= asInt(firstElf[0]) && asInt(secondElf[1]) >= asInt(firstElf[1]) ||
+				asInt(firstElf[0]) <= asInt(secondElf[1]) && asInt(firstElf[1]) >= asInt(secondElf[0])) {
+			overlappingAssignmentPairs += 1
+		}
+	}
+	return fmt.Sprintf("%d", overlappingAssignmentPairs)
+}
+
+func asInt(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return i
+}
