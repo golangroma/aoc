ea96b4262190d400860c816fa181b16cb6cdd252
giuliano <panzironi.giuliano@gmail.com>
2022-12-06 17:15:37 +0100

    solve day06 puzzle

---
+++ 2022/day06/giulianopz/parts_test.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/golangroma/aoc/utils"
+)
+
+var sample string = `bvwbjplbgvbhsrlpgdmjqwftvncz`
+var p2sample = `mjqjpqmgbljsphdztnvjfqwrcgsmlb`
+
+func TestPartOne(t *testing.T) {
+	tt := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{
+			name:     "example",
+			input:    sample,
+			expected: "5",
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			input := utils.SplitInput(tc.input)
+			if got := PartOne(input); got != tc.expected {
+				t.Errorf("PartOne() = %v, want %v", got, tc.expected)
+			}
+		})
+	}
+}
+
+func TestPartTwo(t *testing.T) {
+	tt := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{
+			name:     "example",
+			input:    p2sample,
+			expected: "19",
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			input := utils.SplitInput(tc.input)
+			if got := PartTwo(input); got != tc.expected {
+				t.Errorf("PartTwo() = %v, want %v", got, tc.expected)
+			}
+		})
+	}
+}
