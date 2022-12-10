18c948f2f3206e6a12384e21b1a0f553bcf59e01
giuliano <panzironi.giuliano@gmail.com>
2022-12-10 13:23:39 +0100

    reimplement fs as a graph of pointers

--- 2022/day07/giulianopz/parts.go
+++ 2022/day07/giulianopz/parts.go
@@ -9,136 +9,105 @@ import (
 
 const (
 	maxSize = 100000
-)
 
-type fs []*dir
-
-type dir struct {
-	size     int
-	name     string
-	children []int
-	parent   int
-}
-
-func (fs *fs) insert(d *dir) int {
-
-	dirIdx := fs.search(d.name)
-	if dirIdx != -1 {
-		([]*dir)(*fs)[dirIdx].size = d.size
-		([]*dir)(*fs)[dirIdx].children = d.children
-		return dirIdx
-	}
-	*fs = append(*fs, d)
-	return len(*fs) - 1
-}
-
-func (fs *fs) propagate(fileSize, parentIdx int) {
-	stack := make([]int, 0)
-
-	if parentIdx != -1 {
-		stack = append(stack, parentIdx)
-	}
-
-	for len(stack) != 0 {
-		idx := stack[0]
-		parent := ([]*dir)(*fs)[idx]
-		parent.size += fileSize
-
-		stack = stack[1:]
-		if parent.parent != -1 {
-			stack = append(stack, parent.parent)
-		}
-	}
-}
-
-func (fs *fs) search(name string) int {
-	for i, d := range *fs {
-		if d.name == name {
-			return i
-		}
-	}
-	return -1
-}
-
-const (
 	cd = "cd"
 	ls = "ls"
 
-	dollar     = "$"
-	fsRootName = "/"
+	dollarSign  = "$"
+	fsRootName  = "/"
+	previousDir = ".."
 
 	commandPattern = `^\$\s(\w+)\s?(.*)?`
 	dirPattern     = `^dir\s(.*)$`
 	filePattern    = `(\d+)\s(.*)`
 )
 
+type dir struct {
+	size     int
+	name     string
+	children []*dir
+	parent   *dir
+}
+
+func propagate(fileSize int, parent *dir) {
+	stack := make([]*dir, 0)
+
+	if parent != nil {
+		stack = append(stack, parent)
+	}
+
+	for len(stack) != 0 {
+		p := stack[0]
+		p.size += fileSize
+		stack = stack[1:]
+
+		if p.parent != nil {
+			stack = append(stack, p.parent)
+		}
+	}
+}
+
+func regex(patter string) *regexp.Regexp {
+	regex, err := regexp.Compile(patter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return regex
+}
+
 func PartOne(input []string) string {
 
-	commandRgx, err := regexp.Compile(commandPattern)
-	if err != nil {
-		log.Fatal(err)
-	}
+	commandRgx := regex(commandPattern)
+	dirRgx := regex(dirPattern)
+	fileRgx := regex(filePattern)
 
-	dirRgx, err := regexp.Compile(dirPattern)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fileRgx, err := regexp.Compile(filePattern)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fs := fs{}
-	var current *dir
-	for i, line := range input {
+	var lastDir *dir
+	for _, line := range input {
 
 		if commandRgx.MatchString(line) {
 			groups := commandRgx.FindAllStringSubmatch(line, -1)
 			cmd := groups[0][1]
 			dirName := groups[0][2]
-			if cmd == cd {
 
-				if dirName != ".." {
-					dirIdx := fs.search(dirName)
-					if dirIdx != -1 {
-						current = ([]*dir)(fs)[dirIdx]
-					} else {
-						current = new(dir)
-						current.name = dirName
-						current.parent = -1
-						fs.insert(current)
+			if cmd == ls {
+				continue
+			}
+			if cmd == cd {
+				if lastDir == nil {
+					lastDir = new(dir)
+					lastDir.name = dirName
+				} else {
+					if dirName == previousDir {
+						lastDir = lastDir.parent
+					}
+					for _, c := range lastDir.children {
+						if c.name == dirName {
+							child := c
+							lastDir = child
+							break
+						}
 					}
 				}
 			}
+
 		} else if dirRgx.MatchString(line) {
 			groups := dirRgx.FindAllStringSubmatch(line, -1)
 			childName := groups[0][1]
 
-			childIdx := fs.insert(&dir{name: childName, parent: fs.search(current.name)})
-			current.children = append(current.children, childIdx)
+			child := new(dir)
+			child.name = childName
+			child.parent = lastDir
+			lastDir.children = append(lastDir.children, child)
+
 		} else if fileRgx.MatchString(line) {
 			groups := fileRgx.FindAllStringSubmatch(line, -1)
 			fileSize, _ := strconv.Atoi(groups[0][1])
-			current.size += fileSize
-			fs.propagate(fileSize, current.parent)
-		}
-
-		if i == len(input)-1 {
-			if current != nil {
-				fs.insert(current)
-			}
+			lastDir.size += fileSize
+			propagate(fileSize, lastDir.parent)
 		}
 	}
 
-	var totSize int
-	for _, d := range fs {
-		if d.size <= maxSize {
-			totSize += d.size
-		}
-	}
-
-	return fmt.Sprintf("%d", totSize)
+	return fmt.Sprintf("%d", 0)
 }
 
 func PartTwo(input []string) string {
