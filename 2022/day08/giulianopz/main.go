9eb97b8305beea1feedd2f2da89faf79d509aa0f
Giuliano Panzironi <45801466+giulianopz@users.noreply.github.com>
2022-12-11 19:21:03 +0100

    day 05-09
    Add my solutions* for puzzles from day 05 to day 09.
    
    *I had no time to refactor the code. Hope to find it before xmas.

---
+++ 2022/day08/giulianopz/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/golangroma/aoc/utils"
+)
+
+func main() {
+	input := utils.ReadFile("input.txt")
+
+	fmt.Printf("Part 1: %v\n", PartOne(input))
+	fmt.Printf("Part 2: %v\n", PartTwo(input))
+	/*
+		Part 1: 1672
+		Part 2: 327180
+	*/
+}
---
+++ 2022/day09/giulianopz/parts_test.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/golangroma/aoc/utils"
+)
+
+var sample string = `
+R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2`
+
+var sample2 string = `
+R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20
+`
+
+func TestPartOne(t *testing.T) {
+	tt := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{
+			name:     "example",
+			input:    sample,
+			expected: "13",
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			input := utils.SplitInput(tc.input)
+			if got := PartOne(input); got != tc.expected {
+				t.Errorf("PartOne() = %v, want %v", got, tc.expected)
+			}
+		})
+	}
+}
+
+func TestPartTwo(t *testing.T) {
+	tt := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{
+			name:     "example",
+			input:    sample2,
+			expected: "36",
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			input := utils.SplitInput(tc.input)
+			if got := PartTwo(input); got != tc.expected {
+				t.Errorf("PartTwo() = %v, want %v", got, tc.expected)
+			}
+		})
+	}
+}
