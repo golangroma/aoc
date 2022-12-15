b396ca007aec6f441818199348909c2c289f3026
Michele Mastrogiovanni <michele.mastrogiovanni@gmail.com>
2022-12-15 09:13:36 +0100

    Mastrogiovanni 2022 13 (#69)
    * Mastrogiovanni day 13
    
    * Mastrogiovanni day 13
    
    * Second stage and fix
    
    * Fix according luciano suggestion

---
+++ 2022/day13/mastrogiovanni/parts.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func PartOne(inputs []string) string {
+	tot := 0
+	index := 1
+	for i := 0; i < len(inputs); i += 3 {
+		left := inputs[i]
+		right := inputs[i+1]
+		if res := Compare3(left, right); res {
+			tot += index
+		}
+		index++
+	}
+	return fmt.Sprintf("%d", tot)
+}
+
+func PartTwo(inputs []string) string {
+	list := make([]any, 0)
+	for i := 0; i < len(inputs); i += 3 {
+		left := parse(inputs[i])
+		right := parse(inputs[i+1])
+		list = append(list, left, right)
+	}
+	list = append(list, parse("[[2]]"), parse("[[6]]"))
+	sort.Slice(list, func(i, j int) bool {
+		return compare(list[i], list[j]) < 0
+	})
+	tot := 1
+	for i := 0; i < len(list); i++ {
+		if fmt.Sprintf("%+v", list[i]) == "[[2]]" {
+			tot *= (i + 1)
+		}
+		if fmt.Sprintf("%+v", list[i]) == "[[6]]" {
+			tot *= (i + 1)
+		}
+	}
+	return fmt.Sprintf("%d", tot)
+}
---
+++ 2022/day13/mastrogiovanni/parts_test.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/golangroma/aoc/utils"
+)
+
+var sample string = `[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]`
+
+func TestPartOne(t *testing.T) {
+	tt := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{
+			name:     "example",
+			input:    sample,
+			expected: "13",
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			input := utils.SplitInput(tc.input)
+			if got := PartOne(input); got != tc.expected {
+				t.Errorf("PartOne() = %v, want %v", got, tc.expected)
+			}
+		})
+	}
+}
+
+func TestPartTwo(t *testing.T) {
+	tt := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{
+			name:     "example",
+			input:    sample,
+			expected: "140",
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			input := utils.SplitInput(tc.input)
+			if got := PartTwo(input); got != tc.expected {
+				t.Errorf("PartTwo() = %v, want %v", got, tc.expected)
+			}
+		})
+	}
+}
