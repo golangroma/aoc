31aab9dd19368bdeaa85b61fe530c77a795901c3
Enrico Candino <enrico.candino@gmail.com>
2023-12-01 23:36:34 +0100

    dunno

--- 2022/day07/enrichman/parts.go
+++ 2022/day07/enrichman/parts.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"strconv"
 	"strings"
+
+	"github.com/golangroma/aoc/utils"
 )
 
 type FileType int
@@ -28,23 +32,81 @@ func NewFile(name string, _type FileType, size int) *File {
 	}
 }
 
+func (f *File) CD(dir string) *File {
+	if dir == ".." {
+		return f.Parent
+	}
+
+	for _, f := range f.File {
+		if f.Name == dir {
+			return f
+		}
+	}
+
+	return nil
+}
+
+func (f *File) String() string {
+	return fmt.Sprintf(
+		"File{Name:%s, Size: %d, Type: %v, Parent: %v, Files:%s}",
+		f.Name, f.Size, f.Type, f.Parent, f.File,
+	)
+}
+
 func PartOne(input []string) string {
 	currentDir := NewFile("/", Directory, 0)
 
-	for i := 0; i < len(input); i++ {
-		// handle commands
-		if strings.HasPrefix(line, "$ cd") {
-			dirName := strings.TrimPrefix(s, "$ cd")
-			if dirName == ".." {
-				currentDir = currentDir.Parent
-				continue
-			}
+	for i := 1; i < len(input); i++ {
 
-			newDir := NewFile(dirName, Directory, 0)
+		fmt.Printf("currDir: %+v\n", currentDir)
+
+		line := input[i]
+		fmt.Println(line)
+
+		// handle commands
+		if strings.HasPrefix(line, "$ cd ") {
+			dirName := strings.TrimPrefix(line, "$ cd ")
+
+			newDir := currentDir.CD(dirName)
+			fmt.Printf("a currDir: %+v\n", currentDir)
+
 			newDir.Parent = currentDir
+			fmt.Printf("b currDir: %+v\n", currentDir)
 			currentDir = newDir
+
+			fmt.Printf("c currDir: %+v\n", currentDir)
 			continue
 		}
+
+		if strings.HasPrefix(line, "$ ls") {
+
+			for i < len(input)-1 {
+				i++
+				line = input[i]
+
+				if strings.HasPrefix(line, "$") {
+					i--
+					break
+				}
+
+				lineArr := strings.Fields(line)
+				var newFile *File
+				if lineArr[0] == "dir" {
+					newFile = NewFile(lineArr[1], Directory, 0)
+				} else {
+					size, err := strconv.Atoi(lineArr[0])
+					utils.CheckErr(err)
+
+					newFile = NewFile(lineArr[1], RegularFile, size)
+				}
+
+				fmt.Println("1 AA")
+				newFile.Parent = currentDir
+				currentDir.File = append(currentDir.File, newFile)
+			}
+			fmt.Println("2 AA", currentDir)
+		}
+		fmt.Println("3 AA", currentDir)
 	}
 
 	return ""
@@ -52,32 +114,4 @@ func PartOne(input []string) string {
 
 func PartTwo(input []string) string {
 	return ""
-}
-
-type Cmd interface {
-	Execute(currentDir *File)
-}
-
-type ChangeDirCmd struct{}
-
-func (cd *ChangeDirCmd) Execute(currentDir *File) {
-
-}
-
-type ListCmd struct{}
-
-func (ls *ListCmd) Execute(currentDir *File) {
-	for _, f := range currentDir.File {
-
-	}
-}
-
-func parseCommand(s string) Cmd {
-	cmd := strings.TrimPrefix(s, "$ ")
-	switch cmd {
-	case strings.HasPrefix("cd"):
-		c = &Cmd{}
-	case strings.HasPrefix("ls"):
-		c = &Cmd{}
-	}
 }
