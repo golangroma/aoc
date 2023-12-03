dbe456c54bfea0a03127f787b64d2d973f354509
Enrico Candino <enrico.candino@gmail.com>
2023-12-03 12:02:18 +0100

    enrichman day01

---
+++ 2023/day01/enrichman/parts_test.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/golangroma/aoc/utils"
+)
+
+var sample string = `1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet`
+
+func TestPartOne(t *testing.T) {
+	tt := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{
+			name:     "example",
+			input:    sample,
+			expected: "142",
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			input := utils.SplitInput(tc.input)
+			if got := PartOne(input); got != tc.expected {
+				t.Errorf("PartOne() = %v, want %v", got, tc.expected)
+			}
+		})
+	}
+}
+
+var sample2 string = `two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen`
+
+func TestPartTwo(t *testing.T) {
+	tt := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{
+			name:     "example",
+			input:    sample2,
+			expected: "281",
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			input := utils.SplitInput(tc.input)
+			if got := PartTwo(input); got != tc.expected {
+				t.Errorf("PartTwo() = %v, want %v", got, tc.expected)
+			}
+		})
+	}
+}
