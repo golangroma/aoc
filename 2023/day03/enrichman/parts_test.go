35f0147fa89dff29e2b12898491d01b7fa0552c1
Enrico Candino <enrico.candino@gmail.com>
2023-12-05 00:50:39 +0100

    enrichman day03

---
+++ 2023/day03/enrichman/parts_test.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/golangroma/aoc/utils"
+)
+
+var sample string = `467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..`
+
+func TestPartOne(t *testing.T) {
+	tt := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{
+			name:     "example",
+			input:    sample,
+			expected: "4361",
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			input := utils.SplitInput(tc.input)
+			if got := PartOne(input); got != tc.expected {
+				t.Errorf("PartOne() = %v, want %v", got, tc.expected)
+			}
+		})
+	}
+}
+
+func TestPartTwo(t *testing.T) {
+	tt := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{
+			name:     "example",
+			input:    sample,
+			expected: "467835",
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			input := utils.SplitInput(tc.input)
+			if got := PartTwo(input); got != tc.expected {
+				t.Errorf("PartTwo() = %v, want %v", got, tc.expected)
+			}
+		})
+	}
+}
